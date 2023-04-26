cc614574e1fd626479d758dd75ec4db4c06cad21
Heitor Fernando Barbosa Ramos <heitorhfbr@gmail.com>
2023-04-26 13:25:24 -0300

    Adcionando depencia feign

--- service/src/main/java/com/sisweb/api/MinhaApiDeTesteApplication.java
+++ service/src/main/java/com/sisweb/api/MinhaApiDeTesteApplication.java
@@ -2,8 +2,10 @@ package com.sisweb.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MinhaApiDeTesteApplication {
 
 	public static void main(String[] args) {
