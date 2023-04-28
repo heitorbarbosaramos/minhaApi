d00e34ef61451ee5a2fa4ddab957d86622d6576b
Heitor Fernando Barbosa Ramos <heitorhfbr@gmail.com>
2023-04-28 08:15:01 -0300

    Retirando commandlinerunner

--- service/src/main/java/com/sisweb/api/MinhaApiDeTesteApplication.java
+++ service/src/main/java/com/sisweb/api/MinhaApiDeTesteApplication.java
@@ -1,21 +1,14 @@
 package com.sisweb.api;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableFeignClients
-public class MinhaApiDeTesteApplication implements CommandLineRunner {
+public class MinhaApiDeTesteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MinhaApiDeTesteApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println(new BCryptPasswordEncoder().encode("123456"));
 	}
 }
