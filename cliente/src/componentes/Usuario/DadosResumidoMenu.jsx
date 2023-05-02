d801c45ea52a61bd9a51236cbab4401f30326eb6
Heitor Fernando Barbosa Ramos <heitorhfbr@gmail.com>
2023-05-02 14:59:43 -0300

    Mostrando dados usuario

--- cliente/src/componentes/Usuario/DadosResumidoMenu.jsx
+++ cliente/src/componentes/Usuario/DadosResumidoMenu.jsx
@@ -1,8 +1,20 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from '../../Services/Auth.jsx';
 import "./DadosResumidoMenu.css";
+
 const DadosResumidoMenu = () =>{
 
-    const nome = localStorage.getItem("myApiNome");
-    const login = localStorage.getItem("myApiLogin");
+    const { authenticated } = useContext(AuthContext);
+
+    const [nome, setNome] = useState(localStorage.getItem("myApiNome"))
+    const [login, setLogin] = useState(localStorage.getItem("myApiLogin"))
+
+    useEffect(() => {
+        const nomeExibe = localStorage.getItem("myApiNome") !== null ? localStorage.getItem("myApiNome") : "nome do usuário";
+        const loginExibe = localStorage.getItem("myApiLogin") !== null ? localStorage.getItem("myApiLogin") : "login do usuário";
+        setNome(nomeExibe);
+        setLogin(loginExibe);
+    }, [authenticated])
 
     return(
         <div className="dados">
